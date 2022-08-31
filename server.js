b0bf3d653880765a6cf6329616a062225380ff6b
npsaunders <neilpsaunders@gmail.com>
2022-08-31 09:56:08 -0400

    add css style sheet

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const marsMissions = require("./models/mars_index.js");
 const app = express();
 const port = 3000;
-
+app.use(express.static("public"));
 
 //routes
 
